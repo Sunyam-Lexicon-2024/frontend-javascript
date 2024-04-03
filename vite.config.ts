38b894b61eb6e4559a120ed9ef9c0ea401a04a71
suny-am <visualarea.1@gmail.com>
2024-04-03 09:45:31 +0200

    Fix base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/frontend-javascript/",
   plugins: [react()],
   resolve: {
     alias: {
